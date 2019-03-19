6ee29b8e2091ead9462870ce15a7227f683e116d
Debjani Paul <debjanideb@hotmail.com>
2019-03-18 23:01:17 -0700

    Added Aria roles

--- js/main.js
+++ js/main.js
@@ -178,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('aria-label', `View details for restaurant ${restaurant.name}`); //Adding aria-labels for the 'view Details' button on index page 
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -175,6 +175,7 @@ fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
+  li.classList.add('addColor');
   breadcrumb.appendChild(li);
 }
 
